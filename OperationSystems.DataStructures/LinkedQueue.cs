4bb02a8dac32a0eb4d0ef47b357835e1d29febad
Erkan Taylan <etaylan13@gmail.com>
2016-10-16 22:13:55 +0300

    homework 1

---
+++ OperationSystems.DataStructures/LinkedQueue.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+
+namespace OperationSystems.DataStructures {
+
+    public class LinkedQueue<T> {
+
+        public Node<T> Front { get; set; }
+        public Node<T> Rear { get; set; }
+
+
+        public void Enqueue(T value) {
+            var node = new Node<T>(value);
+            if (Rear == null) {
+                Front = node;
+                Rear = node;
+            } else {
+                Rear.Next = node;
+                Rear = node;
+            }
+        }
+
+        public Node<T> Dequeue() {
+            if (Front == null) {
+                return null;
+            }
+            var target = Front;
+            Front = Front.Next;
+            return target;
+        }
+
+        public override string ToString() {
+            var output = new StringBuilder();
+            var current = Front;
+            while (current != null) {
+                output.Append($"{current.Value.ToString()} ");
+                current = current.Next;
+            }
+            return output.ToString();
+        }
+
+        public bool IsQueueEmpty() {
+            return Front == null;
+        }
+
+    }
+
+    public class Node<T> {
+
+        public Node(T value) {
+            Value = value;
+        }
+
+        public Node<T> Next { get; set; }
+
+        public T Value { get; set; }
+
+    }
+
+}
---
+++ OperationSystems/ProcessControlBlock.cs
@@ -0,0 +1,43 @@
+﻿/*file:///E:/_LESSONS/_isletim_sistemleri/slides/ch3.pdf
+ * page 9
+**/
+using System.Collections.Generic;
+
+namespace OperationSystems {
+
+    public class ProcessControlBlock {
+
+        //Process state - running waiting etc
+        public ProcessStates ProcessState { get; set; }
+
+        //program counter - location of instruction to next execute
+        public ProgramCounter ProgramCounter { get; set; }
+
+        // cpu registers - contents of all processcentric registers
+        public List<Register> Registers { get; set; }
+
+        //cpu scheduling information - proirities, scheduling queue pointers
+
+        //accounting  information - cpu used, clock time elapsed since start, time limits
+
+        // i/o status information - i/o devices allocated to process, list of open files
+
+    }
+
+    public enum ProcessStates {
+        Newbie,
+        Ready,
+        Running,
+        Waiting,
+        Terminated,
+    }
+
+    public class ProgramCounter {
+
+        
+
+    }
+
+    public class Register { }
+
+}
