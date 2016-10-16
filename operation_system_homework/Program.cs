86c8df6f2374bdad58df4f1e485570efe1c6055c
Erkan Taylan <etaylan13@gmail.com>
2016-10-16 18:51:30 +0300

    first week

--- operation_system_homework/Program.cs
+++ operation_system_homework/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
-namespace operation_system_homework
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+using OperationSystems.DataStructures;
+
+namespace operation_system_homework {
+
+    internal static class Program {
+
+        private static void Main(string[] args) {
+            var list = new LinkedQueue<int>();
+            list.Enqueue(1);
+            list.Enqueue(2);
+            list.Enqueue(3);
+            list.Enqueue(4);
+            list.Enqueue(5);
+            list.Enqueue(6);
+            list.Enqueue(7);
+
+            while (!list.IsQueueEmpty()) {
+                Console.WriteLine(list);
+                Console.WriteLine(list.Dequeue().Value);
+            }
+
+            End();
         }
+
+        private static void End() {
+            Console.WriteLine(" - Fin -");
+            Console.WriteLine("Do not press any key!");
+            Console.ReadKey(true);
+            Console.WriteLine("Bye!");
+            Thread.Sleep(500);
+        }
+
     }
+
 }
